ec2f4b6520f6c0183c7fe22d1f9432f5ce80e413
maleeart <tuangphetch@gmail.com>
2026-05-19 12:31:05 +0700

    Create exchange-token.js

---
+++ api/exchange-token.js
@@ -0,0 +1,35 @@
+module.exports = async function handler(req, res) {
+
+  try {
+
+    const code = req.query.code;
+
+    const response = await fetch(
+      "https://www.strava.com/oauth/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          client_id: process.env.STRAVA_CLIENT_ID,
+          client_secret: process.env.STRAVA_CLIENT_SECRET,
+          code,
+          grant_type: "authorization_code",
+        }),
+      }
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+  } catch (err) {
+
+    res.status(500).json({
+      error: err.message,
+    });
+
+  }
+
+}
