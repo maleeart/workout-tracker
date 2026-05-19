eca15835120f5b749f0aa353ffce2991fa03d149
maleeart <tuangphetch@gmail.com>
2026-05-19 12:13:42 +0700

    Update load.js

--- api/load.js
+++ api/load.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     const token = process.env.GITHUB_TOKEN;
     const owner = process.env.GITHUB_OWNER;
