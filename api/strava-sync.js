a53d36188cf6514828d3685b8fdfc4b142d93d7f
maleeart <tuangphetch@gmail.com>
2026-05-19 12:05:40 +0700

    Update strava-sync.js

--- api/strava-sync.js
+++ api/strava-sync.js
@@ -1,9 +1,14 @@
-
 export default async function handler(req, res) {
 
   try {
 
     const accessToken = process.env.STRAVA_ACCESS_TOKEN;
+
+    if (!accessToken) {
+      return res.status(500).json({
+        error: "Missing STRAVA_ACCESS_TOKEN",
+      });
+    }
 
     const response = await fetch(
       "https://www.strava.com/api/v3/athlete/activities?per_page=1",
@@ -14,13 +19,29 @@ export default async function handler(req, res) {
       }
     );
 
-    const activities = await response.json();
+    const data = await response.json();
 
-    const latest = activities[0];
+    // debug response
+    console.log(data);
+
+    if (!Array.isArray(data)) {
+      return res.status(500).json({
+        error: "Strava API Error",
+        strava: data,
+      });
+    }
+
+    if (data.length === 0) {
+      return res.status(200).json({
+        message: "No activities found",
+      });
+    }
+
+    const latest = data[0];
 
     res.status(200).json({
       workout: latest.name,
-      distance: latest.distance / 1000,
+      distance: (latest.distance / 1000).toFixed(2),
       duration: Math.round(latest.moving_time / 60),
       avgHR: latest.average_heartrate,
       calories: latest.calories,
